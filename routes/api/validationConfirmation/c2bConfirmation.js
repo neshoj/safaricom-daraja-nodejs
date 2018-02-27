f48fc24193825c6a50ffcadd3d84b8c3b7b8680d
neshoj <iojiambo@binary.co.ke>
2018-02-27 17:05:12 +0300

    functional c2b request

--- routes/api/validationConfirmation/c2bConfirmation.js
+++ routes/api/validationConfirmation/c2bConfirmation.js
@@ -2,11 +2,81 @@ var express = require('express')
 var c2bConfirmationRouter = express.Router()
 
 var mpesaFunctions = require('../../helpers/mpesaFunctions')
-var C2BTransaction = require('./c2bTransactionModel');
+var C2BTransaction = require('./c2bTransactionModel')
+var CallbackURLModel = require('./c2bCallbackUrlModel')
 var moment = require('moment')
 
 const GENERIC_SERVER_ERROR_CODE = '01'
-const CONFIRMATION_TRANSACTION_ACTION_TYPE = 'confirma'
+const CONFIRMATION_TRANSACTION_ACTION_TYPE = 'confirmation'
+
+
+/**
+ * Find validation request that is being confirmed by current request
+ * @param req
+ * @param res
+ * @param next
+ */
+function findInitialTransaction(req, res, next) {
+    //Invalid body request
+    if (!req.body)
+        mpesaFunctions.handleError(res, 'Invalid request received', GENERIC_SERVER_ERROR_CODE)
+
+    C2BTransaction.findOne({
+        'validation.MSISDN': req.body.MSISDN,
+        'validation.BillRefNumber': req.body.BillRefNumber,
+        'validation.TransID': req.body.TransID
+    }, function (err, validatedTnx) {
+        //Check for error
+        if (err) return mpesaFunctions.handleError(res, 'Transaction not found', GENERIC_SERVER_ERROR_CODE)
+
+
+        console.log('C2B Validation transaction found for %s Bill reference number %s.',
+            validatedTnx.validation.MSISDN, validatedTnx.validation.BillRefNumber);
+        req.body.initial_tnx = validatedTnx;
+        // console.log(JSON.stringify(req.body.initial_tnx))
+        next();
+    });
+}
+
+/**
+ * Prepare and send confirmation message to remote server
+ * @param req
+ * @param res
+ * @param next
+ */
+function sendRequestToRemoteApplication(req, res, next) {
+    //Prepare object
+    var confirmationReq = {
+        transactionType: req.body.TransactionType,
+        action: CONFIRMATION_TRANSACTION_ACTION_TYPE,
+        phone: req.body.MSISDN,
+        firstName: req.body.FirstName,
+        middleName: req.body.MiddleName,
+        lastName: req.body.LastName,
+        OrgAccountBalance: req.body.OrgAccountBalance,
+        amount: req.body.TransAmount,
+        accountNumber: req.body.BillRefNumber,
+        transID: req.body.TransID,
+        time: moment( new Date(req.body.TransTime)).format('YYYY-MM-DD HH:mm:ss')
+    }
+
+    //Find remote URL configuration from database
+    CallbackURLModel.findOne({
+        'shortCode': req.body.BusinessShortCode
+    }, function (err, remoteEndPoints) {
+        //Invalid database response
+        if (!req.body)
+            mpesaFunctions.handleError(res, 'Pay bill ' + req.body.BusinessShortCode + ' remote URLs not registered', GENERIC_SERVER_ERROR_CODE)
+
+        console.log('Transaction report: '+ JSON.stringify(confirmationReq))
+        console.log('Remote end points: '+ JSON.stringify(remoteEndPoints))
+        //Forward to remote server
+        mpesaFunctions.sendCallbackMpesaTxnToAPIInitiator({
+            url: remoteEndPoints.merchant.confirmation,
+            transaction: confirmationReq
+        }, req, res, next)
+    })
+}
 
 
 /**
@@ -16,71 +86,25 @@ const CONFIRMATION_TRANSACTION_ACTION_TYPE = 'confirma'
  * @param next
  */
 var saveTransaction = function (req, res, next) {
-//     var transaction = new C2BTransaction(
-//         {
-//             validation: req.body,
-//             validationResult: req.validationResult
-//         }
-//     )
-// //   persist transaction details
-//     transaction.save(function (err) {
-//         if (err) mpesaFunctions.handleError(req, 'Unable to save validation request.', GENERIC_SERVER_ERROR_CODE)
-//
-//         console.log('Transaction saved...')
+
+    var filter = {
+        'validation.MSISDN': req.body.MSISDN,
+        'validation.BillRefNumber': req.body.BillRefNumber,
+        'validation.TransID': req.body.TransID
+    }
+    console.log('Fetch initial transaction')
+    C2BTransaction.update(filter, {$set: {confirmation: req.body}}, {upsert: true}, function (err) {
+        if (err) mpesaFunctions.handleError(req, 'Unable to save validation request.', GENERIC_SERVER_ERROR_CODE)
+
+        console.log('Transaction updated ')
         next();
-    // })
+    })
+
 }
 
-/**
- * Fetch initial transaction from db, Send confirmation message to remote server
- * @param req
- * @param res
- * @param next
- */
-function sendRequestToRemoteApplication(req, res, next) {
-    //Invalid body
-    if (!req.body)
-        mpesaFunctions.handleError(res, 'Invalid request received', GENERIC_SERVER_ERROR_CODE)
-
-    C2BTransaction.findOne({
-        'validation.MSISDN': req.MSISDN,
-        'validation.BillRefNumber': req.BillRefNumber,
-        'validation.TransID': req.TransID
-    }, function (err, validatedTnx) {
-        if (err) return  mpesaFunctions.handleError(res, 'Transaction not found', GENERIC_SERVER_ERROR_CODE)
-
-        console.log(req.body.initial_tnx)
-
-        console.log('C2B Validation transaction found for %s Bill reference number %s.',
-            validatedTnx.validation.MSISDN, validatedTnx.validation.BillRefNumber);
-
-        req.body.initial_tnx = validatedTnx;
-
-        next();
-
-        // mpesaFunctions.sendCallbackMpesaTxnToAPIInitiator({
-        //     url: '',
-        //     transaction: {
-        //         transactionType: req.body.TransactionType,
-        //         action: CONFIRMATION_TRANSACTION_ACTION_TYPE,
-        //         phone: req.body.MSISDN,
-        //         firstName: req.body.FirstName,
-        //         middleName: req.body.MiddleName,
-        //         lastName: req.body.LastName,
-        //         OrgAccountBalance: req.body.OrgAccountBalance,
-        //         amount: req.body.TransAmount,
-        //         accountNumber: req.body.BillRefNumber,
-        //         transID: req.body.TransID,
-        //         time: moment( req.body.TransTime ).format('YYYY-MM-DD HH:mm:ss')
-        //     }
-        // }, req, res, next)
-
-    });
-
-
-}
 
 c2bConfirmationRouter.post('/',
+    findInitialTransaction,
     sendRequestToRemoteApplication,
     saveTransaction,
     function (req, res, next) {
--- routes/api/validationConfirmation/c2bValidation.js
+++ routes/api/validationConfirmation/c2bValidation.js
@@ -2,38 +2,67 @@ var express = require('express')
 var c2bValidationRouter = express.Router()
 
 var mpesaFunctions = require('../../helpers/mpesaFunctions')
-var C2BTransaction = require('./c2bTransactionModel');
+var C2BTransaction = require('./c2bTransactionModel')
+var CallbackURLModel = require('./c2bCallbackUrlModel')
 const GENERIC_SERVER_ERROR_CODE = '01'
 const VALIDATION_TRANSACTION_ACTION_TYPE = 'validate'
 
+/**
+ * Send request to remote server for account number validation
+ * @param req req.body contains request details from Safaricom
+ * @param res
+ * @param next
+ */
 var validateRequest = function (req, res, next) {
+    //Check request validity
     if (!req.body)
         mpesaFunctions.handleError(res, 'Invalid request received', GENERIC_SERVER_ERROR_CODE)
 
-    mpesaFunctions.sendCallbackMpesaTxnToAPIInitiator({
-        url: '',
-        transaction: {
-            transactionType: req.body.TransactionType,
-            action: VALIDATION_TRANSACTION_ACTION_TYPE,
-            phone: req.body.MSISDN,
-            firstName: req.body.FirstName,
-            middleName: req.body.MiddleName,
-            lastName: req.body.LastName,
-            amount: req.body.TransAmount,
-            accountNumber: req.body.BillRefNumber,
-            time: req.body.TransTime
-        }
-    }, req, res, next)
+    //Package request
+    var validationReq = {
+        transactionType: req.body.TransactionType,
+        action: VALIDATION_TRANSACTION_ACTION_TYPE,
+        phone: req.body.MSISDN,
+        firstName: req.body.FirstName,
+        middleName: req.body.MiddleName,
+        lastName: req.body.LastName,
+        amount: req.body.TransAmount,
+        accountNumber: req.body.BillRefNumber,
+        time: req.body.TransTime
+    }
+
+    //Find remote URL configuration from database
+    CallbackURLModel.findOne({
+        'shortCode': req.body.BusinessShortCode
+    }, function (err, remoteEndPoints) {
+        // Invalid database response
+        if (!req.body)
+            mpesaFunctions.handleError(res, 'Pay bill ' + req.body.BusinessShortCode + ' remote URLs not registered', GENERIC_SERVER_ERROR_CODE)
+
+        // Short code remote end points not found
+        if (!remoteEndPoints)
+            mpesaFunctions.handleError(res, 'Remote end points for ' + req.body.BusinessShortCode + ' not found.', GENERIC_SERVER_ERROR_CODE)
+
+        //Forward to remote server
+        mpesaFunctions.sendCallbackMpesaTxnToAPIInitiator({
+            url: remoteEndPoints.merchant.confirmation,
+            transaction: validationReq
+        }, req, res, next)
+    })
 }
 
-
+/**
+ * Save transaction details to db
+ * @param req
+ * @param res
+ * @param next
+ */
 var saveTransaction = function (req, res, next) {
-    var transaction = new C2BTransaction(
-        {
-            validation: req.body,
-            validationResult: req.transactionResp
-        }
-    )
+    var transaction = new C2BTransaction({
+        validation: req.body,
+        validationResult: req.transactionResp
+    })
+
 //   persist transaction details
     transaction.save(function (err) {
         if (err) mpesaFunctions.handleError(req, 'Unable to save validation request.', GENERIC_SERVER_ERROR_CODE)
@@ -43,16 +72,30 @@ var saveTransaction = function (req, res, next) {
     })
 }
 
+/**
+ * Process response from remote merchant API
+ * @param req req.transactionResp contains the response from the merchant
+ * @param res
+ * @param next
+ */
+function processRemoteValidationResp(req, res, next) {
+    //Check response validity
+    if (!req.transactionResp) mpesaFunctions.handleError(req, 'Validating account reference request failed.', GENERIC_SERVER_ERROR_CODE)
+
+    // Design response to safaricom
+    req.body.safaricomResp = {
+        ResultCode: req.transactionResp.status === '00' ? 0 : 1,
+        ResultDesc: req.transactionResp.message,
+        ThirdPartyTransID: req.transactionResp.transactionId
+    }
+}
 
 c2bValidationRouter.post('/',
     validateRequest,
+    processRemoteValidationResp,
     saveTransaction,
     function (req, res, next) {
-        res.json({
-            ResultCode: 0,
-            ResultDesc: 'Transaction processing successful',
-            ThirdPartyTransID: ''
-        })
+        res.json(req.body.safaricomResp)
     })
 
 
